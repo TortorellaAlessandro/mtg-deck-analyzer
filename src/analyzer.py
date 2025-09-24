09781bea1f00ddb0038ef8066b31d09b8a917bef
TortorellaAlessandro <79978623+TortorellaAlessandro@users.noreply.github.com>
2025-09-24 15:19:48 +0200

    Added two new tabs, card type distribution and stat distribution

--- src/analyzer.py
+++ src/analyzer.py
@@ -5,3 +5,9 @@ def mana_curve(deck: pd.DataFrame) -> pd.Series:
 
 def color_distribution(deck: pd.DataFrame) -> pd.Series:
     return deck.explode("colors").groupby("colors")["quantity"].sum()
+
+def type_distribution(deck: pd.DataFrame) -> pd.Series:
+    return deck.groupby("type")["quantity"].sum()
+
+def creatures_stats(deck: pd.DataFrame) -> pd.Series:
+    return deck[deck["power/toughness"] != "-1/-1"].groupby("power/toughness")["quantity"].sum()
--- src/main.py
+++ src/main.py
@@ -5,23 +5,43 @@ import sys
 
 from deck_loader import load_deck, read_deck
 from scryfall_api import get_card_info
-from analyzer import mana_curve, color_distribution
+from analyzer import mana_curve, color_distribution, creatures_stats, type_distribution
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+def determine_card_type(type_line: str)-> str:
+    target = type_line.lower()
+    if "creature" in target:
+        return "Creature"
+    elif "artifact" in target:
+        return "Artifact"
+    elif "enchantment" in target:
+        return "Enchantment"
+    elif "instant" in target:
+        return "Instant"
+    elif "sorcery" in target:
+        return "Sorcery"
+    elif "planeswalker" in target:
+        return "Planeswalker"
+    elif "land" in target:
+        return "Land"
+    else:
+        return "Other"
 
 def add_info(deck):
     cards = []
     for _, card in deck.iterrows():
         info = get_card_info(card["name"])
         if "error" not in info:
-            if "colors" not in info:
-                info["colors"] = "Colorless"
+            
             cards.append(
                 {
                     "name": info["name"],
                     "quantity": card["quantity"],
                     "cmc": info["cmc"],
-                    "colors": info["colors"]
+                    "colors": info.get("colors", "Colorless"),
+                    "power/toughness": info.get("power", "-1") + "/" + info.get("toughness", "-1"),
+                    "type": determine_card_type(info["type_line"])
                 })
     return cards
 
@@ -55,16 +75,15 @@ if __name__ == "__main__":
 
         df = pd.DataFrame(enriched_deck)
 
-        curve = mana_curve(df)
-        colors = color_distribution(df)
-
         graphs_ready = True
 
-    tab_curve, tab_colors = st.tabs(["Mana Curve", "Color Distribution"])
-
-    with tab_curve:
-        if graphs_ready:
-            st.bar_chart(curve)
-    with tab_colors:
-        if graphs_ready:
-            st.bar_chart(colors)
+    tab_curve, tab_colors, tab_type, tab_stats = st.tabs(["Mana Curve", "Color Distribution","Type Distribution", "Stats Distribution"])
+    if graphs_ready:
+        with tab_curve:
+            st.bar_chart(mana_curve(df))
+        with tab_colors:
+            st.bar_chart(color_distribution(df))
+        with tab_type:
+            st.bar_chart(type_distribution(df))
+        with tab_stats:
+            st.bar_chart(creatures_stats(df))
